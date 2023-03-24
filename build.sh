059a3d83817e3d64dda1681fcc08711d6ef3e7df
Jakub Jabůrek <jaburek.jakub@gmail.com>
2023-03-24 13:18:09 +0100

    fix: correct edit_url for enginiro/docs

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ fi
 sed -i -E '/^  (- url|  start_path|  edit_url):/d' antora-playbook.yml
 echo "  - url: ./" >> antora-playbook.yml
 echo "    start_path: docs" >> antora-playbook.yml
-echo "    edit_url: 'https://github.com/enginiro/$name/edit/{refname}/{path}'" >> antora-playbook.yml
+echo "    edit_url: 'https://github.com/enginiro/docs/edit/{refname}/{path}'" >> antora-playbook.yml
 
 mkdir -p build/site/phpdoc/
 
